7bc856fa30c010126e816f1c813e68ce089df8c5
vudala <vudalae@gmail.com>
2022-02-25 13:26:54 -0300

    Dando uso ao retorno do fgets

--- utils.h
+++ utils.h
@@ -9,6 +9,9 @@ void must_alloc(void* ptr, const char* label);
 // Verifica se F foi aberto, caso contrário dispara uma mensagem em stderr e termina a execução com código 1
 void must_open(FILE* f, const char* fname);
 
+// Dispara um erro incondicionalmente
+void throw_exception(const char* func);
+
 // Inverte uma string
 char* str_reverse(char* str);
 
